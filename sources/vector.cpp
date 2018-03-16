36d3f212788e33bf2d53f9964b1ad363f089d003
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-03-16 11:08:53 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -5,10 +5,19 @@
 
 vector_t::vector_t()
 {
+	int * elements_ = new int*[];
+	size_=0;
+	capacity_=0;
 }
 
 vector_t::vector_t(vector_t const & other)
 {
+	size_ = other.size();
+	capacity_ = other.capacity();
+	int * elements_ = new int*[size];
+	for(int i = 0; i < other.size(); ++i) {
+		elements_[i] = other[i];	
+	}
 }
 
 vector_t & vector_t::operator =(vector_t const & other)
@@ -46,7 +55,7 @@ void vector_t::pop_back()
 
 int & vector_t::operator [](std::size_t index)
 {
-	return elements_[0];
+	return elements_[index];
 }
 
 int vector_t::operator [](std::size_t index) const
