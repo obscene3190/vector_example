81899a291ddecedd65bea8b40be6363e92d6f5a3
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-03-16 11:28:55 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -3,82 +3,74 @@
 
 #include "vector.hpp"
 
-vector_t::vector_t()
-{
-	lements_ = nullptr;
-	size_=0;
-	capacity_=0;
-}
+vector_t::vector_t() {
+        elements_ = nullptr;
+        size_ = 0;
+        capacity_ = 0;
+    }
 
-vector_t::vector_t(vector_t const & other)
-{
-	size_ = other.size();
-	capacity_ = other.capacity();
-	elements_ = new int*[size];
-	for(int i = 0; i < other.size(); ++i) {
-		elements_[i] = other[i];	
-	}
-}
+    vector_t::vector_t(vector_t const &other) {
+        size_ = other.size_;
+        capacity_ = other.capacity_;
+        elements_ = new int[capacity_];
+        for (int i = 0; i < other.size_; ++i) {
+            elements_[i] = other.elements_[i];
+        }
+    }
 
-vector_t & vector_t::operator =(vector_t const & other)
-{
-	delete[] elements;
-	size_ = other.size();
-	capacity_ = other.capacity();
-	int * elements_ = new int*[size];
-	for(int i = 0; i < other.size(); ++i) {
-		elements_[i] = other[i];	
-	}
-	return *this;
-}
+    vector_t &vector_t::operator=(vector_t const &other) { 
+	delete[] elements_;
+        size_ = other.size_;
+        capacity_ = other.capacity_;
+        int *elements_ = new int [capacity_];
+        for (int i = 0; i < other.size_; ++i) {
+            	elements_[i] = other.elements_[i];
+        }	
+        return *this;
+    }
 
-bool vector_t::operator ==(vector_t const & other) const
-{
-	if (!(size_ == other.size())) {
-		return false;
-	}
-	for(int i = 0; i < size ++i) {
-		if (!(elements_[i] == other[i])) {
-			return false;
-		}
-	}
-	return false;
-}
+    bool vector_t::operator==(vector_t const &other) const {
+	if (size_ != other.size_) {
+            return false;
+        }
+        for (int i = 0; i < size_; ++i) {
+            if (!(elements_[i] == other.elements_[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-vector_t::~vector_t()
-{
-}
+    vector_t::~vector_t() {
+        for (int i = 0; i < size_; ++i) {
+            delete elements_[i];
+        }    
+        delete[] elements_;
+    }
 
-std::size_t vector_t::size() const
-{
-    return size_;
-}
+    std::size_t vector_t::size() const {
+        return size_;
+    }
 
-std::size_t vector_t::capacity() const
-{
-    return capacity_;
-}
+    std::size_t vector_t::capacity() const {
+        return capacity_;
+    }
 
-void vector_t::push_back(int value)
-{
-	
-}
+    void vector_t::push_back(int value) {
+	    
+    }
 
-void vector_t::pop_back()
-{
-}
+    void vector_t::pop_back() {
+    }
 
-int & vector_t::operator [](std::size_t index)
-{
-	return elements_[index];
-}
+    int &vector_t::operator[](std::size_t index) {
+        return elements_[index];
+    }
 
-int vector_t::operator [](std::size_t index) const
-{
-	return 0;
-}
+    int vector_t::operator[](std::size_t index) const {
+        return 0;
+    }
 
-bool operator !=(vector_t const & lhs, vector_t const & rhs)
-{
-	return true;
-}
+    bool operator!=(vector_t const &lhs, vector_t const &rhs) {
+        return true;
+    }
